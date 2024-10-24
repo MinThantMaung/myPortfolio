4d5acc3b3a199520964ea31e591727368831edc5
MinThant <minthant180@gmail.com>
2024-10-24 16:35:47 +0900

    fix:text font changes

--- src/app/[locale]/components/menu/page.js
+++ src/app/[locale]/components/menu/page.js
@@ -12,23 +12,23 @@ const Menu = ({ onClose }) => {
             <div onClick={handleMenuClose} className="bg-white flex justify-end pt-5 pr-5">
                 <Image src={close} alt="" className="w-7 h-7"/>
             </div>
-            <div className="flex flex-col justify-center items-center h-full">
-                <a href="#home" className="font-bold text-3xl" onClick={handleMenuClose}>
+            <div className="flex flex-col justify-center items-center h-full text-xl">
+                <a href="#home" className="font-bold" onClick={handleMenuClose}>
                     Home
                 </a>
-                <a href="#about" className="font-bold text-3xl mt-6" onClick={handleMenuClose}>
+                <a href="#about" className="font-bold mt-6" onClick={handleMenuClose}>
                     About
                 </a>
-                <a href="#timeline" className="font-bold text-3xl mt-6" onClick={handleMenuClose}>
+                <a href="#timeline" className="font-bold mt-6" onClick={handleMenuClose}>
                     Career
                 </a>
-                <a href="#skill" className="font-bold text-3xl mt-6" onClick={handleMenuClose}>
+                <a href="#skill" className="font-bold mt-6" onClick={handleMenuClose}>
                     Skills
                 </a>
-                <a href="#project" className="font-bold text-3xl mt-6" onClick={handleMenuClose}>
+                <a href="#project" className="font-bold mt-6" onClick={handleMenuClose}>
                     Projects
                 </a>
-                <a href="#contact" className="font-bold text-3xl mt-6" onClick={handleMenuClose}>
+                <a href="#contact" className="font-bold mt-6" onClick={handleMenuClose}>
                     Contact
                 </a>
             </div>
