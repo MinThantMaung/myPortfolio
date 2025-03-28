859efe6771d4fae49df08277a3e0a7ffbc321f5f
MinThantMaung <minthant180@gmail.com>
2025-03-28 17:21:39 +0900

    fix:make api key dynamic

--- src/app/[locale]/components/map/page.js
+++ src/app/[locale]/components/map/page.js
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react';
 import { GoogleMap, LoadScript } from "@react-google-maps/api";
+import { GOOGLE_MAPS_API_KEY } from '../../../../../utils/apiKeys';
 
 const center = {
   lat: 31.91625355541401,
@@ -11,7 +12,7 @@ const Map = () => {
   return (
     <div className="w-full max-w-4xl mx-auto mt-4 md:pr-32 sm:pr-8 pr-8 ml-4 sm:ml-0">
       <div className="relative w-full h-72 sm:h-52 md:h-52 lg:h-[300px] rounded-xl overflow-hidden shadow-lg">
-        <LoadScript googleMapsApiKey="AIzaSyD7BMjTpx6-S3Sc563Ryf3TdUsdeeZPs5s">
+        <LoadScript googleMapsApiKey= {GOOGLE_MAPS_API_KEY}>
           <GoogleMap
             mapContainerStyle={{ width: '100%', height: '100%' }}
             center={center}
