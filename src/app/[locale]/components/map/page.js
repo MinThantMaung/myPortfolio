fb82dd028fef2da1092c43c3774c7ac4c7f32dc8
MinThantMaung <minthant180@gmail.com>
2025-03-28 17:18:00 +0900

    feat:implement map page

---
+++ src/app/[locale]/components/map/page.js
@@ -0,0 +1,28 @@
+'use client'
+import React from 'react';
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
+
+const center = {
+  lat: 31.91625355541401,
+  lng: 131.42272899976493,
+};
+
+const Map = () => {
+  return (
+    <div className="w-full max-w-4xl mx-auto mt-4 md:pr-32 sm:pr-8 pr-8 ml-4 sm:ml-0">
+      <div className="relative w-full h-72 sm:h-52 md:h-52 lg:h-[300px] rounded-xl overflow-hidden shadow-lg">
+        <LoadScript googleMapsApiKey="AIzaSyD7BMjTpx6-S3Sc563Ryf3TdUsdeeZPs5s">
+          <GoogleMap
+            mapContainerStyle={{ width: '100%', height: '100%' }}
+            center={center}
+            zoom={10}
+          >
+            {/* Add markers or children here */}
+          </GoogleMap>
+        </LoadScript>
+      </div>
+    </div>
+  );
+};
+
+export default Map;
